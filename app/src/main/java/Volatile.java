afec5ead6c35168890fc505cff3488e996de1ddb
snpmyn <snpmyn@126.com>
2020-10-13 10:02:49 +0800

    Volatile

---
+++ app/src/main/java/Volatile.java
@@ -0,0 +1,11 @@
+/**
+ * Created on 2020-10-13
+ *
+ * @author zsp
+ * @desc Volatile
+ */
+public class Volatile {
+    public static void main(String... args){
+        System.out.println("RUN");
+    }
+}
